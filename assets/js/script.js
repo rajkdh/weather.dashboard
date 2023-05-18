7784ceee84ee103a2e49849329bb41b385e1168c
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-18 01:27:11 -0400

    updated html code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,9 @@
 var APIKey = "1afe52a96aafad62ee177005ae870f46";
 var city;
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-fetch(queryURL);
+
+
+var currentWeather = function(city) {
+    fetch(c)
+}
+
