9161f09e941ae2b7d1cb1624a867aba72c00320b
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-18 23:17:02 -0400

    updated and cleaned html code. added api key to js and added code to pull current weather.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
-
-var APIKey = "1afe52a96aafad62ee177005ae870f46";
+var key = "1afe52a96aafad62ee177005ae870f46";
 var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+//var forcastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + key key + '&units=metric';
+
+function currentWeather( city ) {
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key + '&units=metric'
+    fetch(queryURL)
+    .then(function(response) { return response.json() })
+    .then(function(data) {
+        displayWeather(data);
+    })
+    .catch(function() {
+
+    });  
+}
+function displayWeather(data) {
+   
+    document.getElementById('current-city').innerHTML = data.name;
+    document.getElementById('current-weather-icon').innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png"/>';
+    document.getElementById('current-temp').innerHTML = data.main.temp + ' &deg;';
+    document.getElementById('current-wind').innerHTML = data.wind.speed + " km/h";
+    document.getElementById('current-humid').innerHTML = data.main.humidity + ' %';
 
 
-var currentWeather = function(city) {
-    fetch(c)
 }
 
+
+window.onload = function() {
+    currentWeather('Toronto')
+
+}
