e076e037a38eb0b0727f53d6dbf0d460fb25f95b
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-21 05:28:11 -0400

    added function to fetch weather data (daily and forecast)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,16 @@
 var key = "161b51b7bfb7bc374ecd556438a97553";
 var city;
-//var forcastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + key key + '&units=metric';
+var searchHistory=[];
 
-function weather( city ) {
-    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + key + '&units=metric'
-    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&cnt=5&appid=" + key + '&units=metric'
-    fetch(queryURL).then(function(response) { return response.json() });
-    fetch(forecastURL).then(function(response) { return response.json() });
+function getWeather(city) {
+var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units=metric`;
+var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
 
-    displayWeather(data);  
-};
-function displayWeather(data) {
-   
-    document.getElementById('current-city').innerHTML = data.name;
-    document.getElementById('current-date').innerHTML = dayjs().format('MMMM D, YYYY')
-    document.getElementById('current-weather-icon').innerHTML = '<img src="https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png"/>';
-    document.getElementById('current-temp').innerHTML = data.main.temp + ' &deg;' + 'C';
-    document.getElementById('current-wind').innerHTML = data.wind.speed + " km/h";
-    document.getElementById('current-humid').innerHTML = data.main.humidity + ' %';
+    fetch(forecastURL).then((resp) => { return resp.json()}).then(function(data){
+        console.log(data);
+    })
+    fetch(weatherURL).then((resp) => {return resp.json()});
 
 }
 
-
-window.onload = function() {
-    weather('Toronto')
-
-}
+getWeather('Toronto');
