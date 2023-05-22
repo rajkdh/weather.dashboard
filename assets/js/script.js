482d9f0633a3bcee50cf92ac59f8ef913f495481
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-22 02:41:22 -0400

    updated js code, displayed current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,44 @@
-var key = "161b51b7bfb7bc374ecd556438a97553";
+const key = "161b51b7bfb7bc374ecd556438a97553";
 var city;
 var searchHistory=[];
 
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units=metric`;
+
 function getWeather(city) {
-var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units=metric`;
-var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
-
-    fetch(forecastURL).then((resp) => { return resp.json()}).then(function(data){
-        console.log(data);
+    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`;
+    fetch(weatherURL)
+    .then((response) => {
+        return response.json();
     })
-    fetch(weatherURL).then((resp) => {return resp.json()});
+    .then((data) => {
+        document.getElementById('current-city').innerHTML = data.name;
+        document.getElementById('current-weather-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/` + data.weather[0].icon + `@2x.png">`;
+        document.getElementById('current-temp').innerHTML = data.main.temp + ' &#8451';
+        document.getElementById('current-wind').innerHTML = data.wind.speed + ' km/h';
+        document.getElementById('current-humid').innerHTML = data.main.humidity + ' %';
+    })
+      
+};
 
-}
+
+
 
 getWeather('Toronto');
+
+
+
+
+
+
+
+
+
+// function displayWeather(d){
+//          document.getElementById('current-city').innerHTML = d.name; 
+// };
+window.onload = function(){
+    getWeather('Toronto');
+//     getWeather('Toronto');
+//     displayWeather()
+//     displayWeather();
+}
